f02990b2d29b364014d9855c07994c81e4268601
TimLeeHoh <82157089+TimLeeHoh@users.noreply.github.com>
2021-04-27 12:38:15 +0200

    Delete zw2.tsx

--- RH_KICKOFF_V1/zw2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="zw2" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
- <image source="zw2.png" width="1024" height="2048"/>
-</tileset>
